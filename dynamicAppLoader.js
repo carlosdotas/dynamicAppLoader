8cfee9d3a39985d967e013a1a2001a90514d6220
Carlos Dotas <42626583+carlosdotas@users.noreply.github.com>
2024-07-12 01:18:00 -0300

    Update dynamicAppLoader.js
    implementacao de eventos em body

--- dynamicAppLoader.js
+++ dynamicAppLoader.js
@@ -16,7 +16,9 @@
                 filesScript: [],
                 modules: {},
                 filesStylesheets: [],
-                onLoad: () => {}
+                onLoad: () => {},
+                loadingShow: () => {},
+                loadingHidden: () => {},
             }, options);
             this.resourcesLoaded = 0;
             this.totalResources = this.settings.filesScript.length + this.settings.filesStylesheets.length;
@@ -31,27 +33,13 @@
                     this.checkUrlForModule(); // Verificar a URL para o módulo após carregar os recursos
                     this.monitorHashChange(); // Monitorar mudanças na hash da URL
                 });
-                //const homePage = this.settings.homePage;                
-                //window.location.href = homePage;                     
-                // Definindo um monitor para uma função chamada "minhaFuncao"
 
                 // Garantir que o namespace easyui existe
                 const homePage = this.settings.homePage; 
 
-                // Função que verifica a criação de window.$.fn.dialog
-                function monitorDialogCreation() {
-                    const intervalId = setInterval(() => {
-                        if (window.$ && window.$.fn && typeof window.$.fn.dialog === 'function') {
-                            clearInterval(intervalId);
-
-                            window.location.href = homePage; 
-                        }
-                    }, 10); // Verifica a cada 100ms
-                }
-
-                // Inicia o monitoramento
-                monitorDialogCreation();
-
+                this.waitForFunction('dialog',function() {
+                     window.location.href = homePage; 
+                })
 
             });
         }
@@ -60,13 +48,77 @@
             return this.settings;
         }
 
-        options(key, options) {
-            if (key && options) {
-                this.settings.modules[key] = {...this.settings.modules[key],...options};
-                if (typeof options.onLoad === 'function') {
-                    options.onLoad(this.settings.modules[key]);
-                }
+        options(options) {
+
+            this.settings.modules[options.id] = {...this.settings.modules[options.id],...options};
+
+            const $component = this.settings.modules[options.id].component;
+
+            if (typeof options.onLoad === 'function') {
+                options.onLoad(this.settings.modules[options.id]);
             }
+
+            const addEventListeners = (actions, selectorModifier = '', preventDefault = false) => {
+                if (Array.isArray(actions)) {
+                    actions.forEach(action => {
+                        Object.keys(action).forEach(key => {
+                            const match = key.match(/\[(.*?)\]/);
+                            if (match && typeof action[key] === 'function') {
+                                const eventType = match[1];
+                                const selector = key.replace(match[0], '');
+                                // Adicionando o evento ao componente com event delegation
+                                $($component).on(eventType, selector + selectorModifier, function(event) {
+                                    if (preventDefault) event.preventDefault();
+                                    action[key].call(this, event);
+                                });
+                            }
+                        });
+                    });
+                }
+            };
+
+            // Lista de todos os eventos possíveis
+            const allEvents = [
+                // Eventos do mouse
+                'click', 'dblclick', 'mousedown', 'mouseup', 'mouseover', 'mousemove', 'mouseout',
+                'mouseenter', 'mouseleave', 'contextmenu',
+
+                // Eventos de teclado
+                'keydown', 'keypress', 'keyup',
+
+                // Eventos de formulário
+                'focus', 'blur', 'change', 'input', 'submit', 'reset',
+
+                // Eventos de janela/documento
+                'load', 'unload', 'beforeunload', 'resize', 'scroll', 'hashchange', 'popstate',
+
+                // Eventos de mídia
+                'abort', 'canplay', 'canplaythrough', 'durationchange', 'emptied', 'ended', 'error',
+                'loadeddata', 'loadedmetadata', 'loadstart', 'pause', 'play', 'playing', 'progress',
+                'ratechange', 'seeked', 'seeking', 'stalled', 'suspend', 'timeupdate', 'volumechange', 'waiting',
+
+                // Eventos de drag & drop
+                'drag', 'dragend', 'dragenter', 'dragleave', 'dragover', 'dragstart', 'drop',
+
+                // Eventos de seleção
+                'select',
+
+                // Eventos de toque
+                'touchstart', 'touchmove', 'touchend', 'touchcancel',
+
+                // Eventos de ponteiro
+                'pointerdown', 'pointerup', 'pointermove', 'pointerover', 'pointerout', 'pointerenter',
+                'pointerleave', 'pointercancel',
+
+                // Outros eventos
+                'animationstart', 'animationend', 'animationiteration', 'transitionstart', 'transitionend',
+                'transitionrun', 'transitioncancel'
+            ];
+
+            // Adicionando os event listeners
+            addEventListeners(options.onClick, '$component.find');
+            addEventListeners(options.onKeyPress, '', true);
+            addEventListeners(options.events); 
         }
 
         getModule(key) {
@@ -114,8 +166,10 @@
         }
 
         addContent(content, parent, url) {
-            let component = $(content);
-            let parentElement;
+
+
+            var component = $(content);
+            var parentElement;
 
             if (parent) {
                 if (typeof parent === 'string') {
@@ -132,34 +186,65 @@
             } else {
                 $('body').append(component);
             }
-            component.attr("id", url);
-            this.settings.modules[url] = {id:url,component:component};
+
+            //component.attr("id", url);
+
+
+            this.settings.modules[url] = { url: url, component: component };
             return component;
         }
 
-        addModulo(url, parent) {
-            if (url) {
-                $.get(url, (data) => {
-                    const tempDiv = $('<div>').html(data);
-                    const scripts = tempDiv.find('script').remove();
-                    const content = tempDiv.html();
-                    
-                    this.addContent(content, parent, url);
 
-                    scripts.each((index, script) => {
-                        const scriptTag = document.createElement('script');
-                        scriptTag.type = 'text/javascript';
-                        if (script.src) {
-                            scriptTag.src = script.src;
-                        } else {
-                            scriptTag.textContent = $(script).text();
+        addModulo(url, parent) {
+            if (url) {             
+                this.settings.loadingShow();   
+                $.get(url)
+                    .done((data) => {
+                        this.settings.loadingHidden();
+                        const tempDiv = $('<div>').html(data);
+                        const scripts = tempDiv.find('script').remove();
+                        const content = tempDiv.html();
+  
+
+                        const textContent = $(scripts[0]).text();
+                        const idMatch = textContent.match(/id:\s*'([^']+)'/);
+                        const idModule = idMatch ? idMatch[1] : 'null';
+
+                        if(!idModule){
+                            idModule = 'dialog_'+Math.floor(Math.random() * 10);
+                            this.settings.modules;
                         }
-                        document.body.appendChild(scriptTag);
+                      
+
+                        if(idModule)this.settings.modules[idModule] = {}  
+
+
+
+                        this.addContent(content, parent, idModule);
+
+
+
+                        scripts.each((index, script) => {
+                            const scriptTag = document.createElement('script');
+                            scriptTag.type = 'text/javascript';
+                            if (script.src) {
+                                scriptTag.src = script.src;
+                            } else {
+                                scriptTag.textContent = $(script).text();
+                            }      
+
+                            document.body.appendChild(scriptTag);
+                        });
+
+
+                    })
+                    .fail(() => {
+                        this.settings.loadingHidden();
+                        console.error('Erro: não foi possível carregar o arquivo:', url);
+                        alert('Erro: não foi possível carregar o arquivo: ' + url);
                     });
-                });
             }
         }
-
 
         monitorHashChange() {
             window.addEventListener('hashchange', () => {
@@ -256,6 +341,15 @@
         GET() {
             return this.urlParams;
         }
+
+        waitForFunction(funcao,callback) {
+            const intervalId = setInterval(() => {
+                if (typeof window.$.fn[funcao] === 'function') {
+                    clearInterval(intervalId);
+                    callback();
+                }
+            }, 10); // Verifica a cada 100ms
+        };
     }
 
     $.apps = function(methodOrOptions, ...params) {
@@ -285,7 +379,6 @@
             const app = $(document).data('appInstance');
             if (app) {
 
-                app.settings.modules[url] = {};
                 app.urlParams = params; // Armazenar os parâmetros da URL
                 app.addModulo(url.split('?')[0], parent ? `#${decodeURIComponent(parent)}` : undefined); // Decodificar e passar o seletor do componente pai
 
